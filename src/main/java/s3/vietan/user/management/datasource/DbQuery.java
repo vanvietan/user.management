a324bd4252e7e24b41430ce7f4cd21ec7b28860e
vanvietan <vanvietan2911@gmail.com>
2022-08-03 10:29:41 +0700

    day3-review

--- src/main/java/s3/vietan/user/management/datasource/DbQuery.java
+++ src/main/java/s3/vietan/user/management/datasource/DbQuery.java
@@ -3,7 +3,7 @@ package s3.vietan.user.management.datasource;
 public class DbQuery {
 	
 	/* User */
-	public static final String findAll ="SELECT id, name, username, password, phone_number, address, age FROM myuser";		
+	public static final String findAll ="SELECT * FROM myuser";		
 	public static final String findById ="SELECT id, name, username, password, phone_number, address, age FROM myuser WHERE id =?";
 	public static final String findByUsername ="SELECT id, name, username, password, phone_number, address, age FROM myuser WHERE username =?";
 	public static final String save ="INSERT INTO myuser (name, username, password, phone_number, address, age) VALUES(?, ?, ?, ?, ?, ?)";
